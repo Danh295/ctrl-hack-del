b99dfe0c4893491fa7442d2cc218aa46880479af
Danny <79547297+Danh295@users.noreply.github.com>
2026-02-22 16:48:04 -0500

    refactor: minor formatting typo

--- ctrl-hack-del/components/ToastProvider.tsx
+++ ctrl-hack-del/components/ToastProvider.tsx
@@ -47,7 +47,7 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
       {children}
       
       {/* Toast Container */}
-      <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-[9999] flex flex-col gap-2 w-full max-w-sm px-4 pointer-events-none">
+      <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-9999 flex flex-col gap-2 w-full max-w-sm px-4 pointer-events-none">
         {toasts.map((toast) => (
           <div
             key={toast.id}
